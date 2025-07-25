262b59fb615e8f052d9036d66e0a39c7ec799f26
Amos Tan <amtan@c2r7s4.42singapore.sg>
2025-07-25 16:24:29 +0800

    3

---
+++ ft_atoi.c
@@ -0,0 +1,25 @@
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	sum;
+	int	sign;
+
+	i = 0;
+	sum = 0;
+	sign = 1;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		sum = (sum * 10) + (str[i] - '0');
+		i++;
+	}
+	return (sign * sum);
+}
+
